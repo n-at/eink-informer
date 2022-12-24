ab7f5642d6b9a88afcdcca041469dbc27d95920f
Alexey Nurgaliev <atnurgaliev@gmail.com>
2022-12-24 20:28:56 +0300

    [WIP] query weather

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"github.com/briandowns/openweathermap"
 	"github.com/mmcdole/gofeed"
 	log "github.com/sirupsen/logrus"
 	"os"
@@ -9,7 +11,11 @@ import (
 
 func main() {
 	verbose := flag.Bool("verbose", false, "show extended output")
-	feedUrl := flag.String("feed", "https://tass.ru/rss/v2.xml", "New feed (RSS, Atom)")
+	feedUrl := flag.String("feed", "https://tass.ru/rss/v2.xml", "News feed (RSS, Atom)")
+	weatherApiKey := flag.String("weather-api-key", "", "openweathermap.org API key, required")
+	weatherLanguage := flag.String("weather-language", "ru", "weather display language")
+	weatherUnits := flag.String("weather-units", "C", "weather measurement system, one of: C, F, K")
+	weatherLocation := flag.String("weather-location", "Pskov, Russia", "weather location name")
 	flag.Parse()
 
 	//prepare logger
@@ -24,7 +30,10 @@ func main() {
 	}
 
 	//rss
-	log.Info("reading feed")
+	if len(*feedUrl) == 0 {
+		log.Fatal("feed url required")
+	}
+	log.Info("read feed")
 	feedParser := gofeed.NewParser()
 	feed, err := feedParser.ParseURL(*feedUrl)
 	if err != nil {
@@ -35,7 +44,22 @@ func main() {
 	}
 
 	//weather
+	if len(*weatherApiKey) == 0 {
+		log.Fatal("openweathermap.org API key required")
+	}
+	log.Info("read weather")
+
+	currentWeather, err := openweathermap.NewCurrent(*weatherUnits, *weatherLanguage, *weatherApiKey)
+	if err != nil {
+		log.Fatalf("unable to get current weather: %s", err)
+	}
+	//if err := currentWeather.CurrentByName(*weatherLocation); err != nil {
+	//	log.Fatalf("unable to get current weather by location: %s", err)
+	//}
+
 	//TODO
+	fmt.Println(weatherLocation)
+	fmt.Println(currentWeather)
 
 	//draw
 	//TODO
